f4787d0e6ad71b7de4abf5542ae84fc9db68922f
Carlene <carlenew.27@gmail.com>
2022-05-06 11:57:48 -0400

    added script to split quals and responsibilities

---
+++ job_scraper/run.py
@@ -0,0 +1,12 @@
+from scraper import main  
+import find_urls as find
+import open_job_links as open
+import filter_job_posting as filter
+import pandas as pd
+
+def concat_job_details():
+    
+
+def create_csv:
+
+main()
