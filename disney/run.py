dd6a8e9d130db44a595229c028e2c84844ff08a3
Carlene <carlenew.27@gmail.com>
2022-05-15 18:08:14 -0400

    rearranging, also added disney job scraper

--- disney/run.py
+++ disney/run.py
@@ -1,5 +1,6 @@
-from scraper import write_HTML_to_file
 from scraper import scrape_HTML
+from clean_jobs import separate
+from clean_jobs import split_and_clean
 import pandas as pd
 
 # Open Chrome, search for "data" on Meta Jobs site
@@ -18,18 +19,6 @@ def create_csv():
 
 
 if __name__ == "__main__":
-    # save_file = open('source_holder.txt', "r+") 
-    # job_data = scraper.scrape_HTML(scraper.launchBrowser(), save_file)
-    job_data = write_HTML_to_file(scrape_HTML())
-    print(job_data)
-    # paths = find_urls.grab_job_paths(job_data)
-    # links = find_urls.add_meta_url(paths)
-    # print(links)
-    # open_job_links.grab_job_data_from_multiple_links(links[:3])
-    # descriptions, responsibilities, qualifications = filter_job_posting.filter_posting(open_job_links.description_file)
-    # print(descriptions)
-    # print(responsibilities)
-    # print(qualifications)
-    # job_data.seek(0)
-    # job_data.truncate(0)
-    # job_data.close()
+    job_data = scrape_HTML()
+    jobs = split_and_clean(job_data)
+    print(separate(jobs))
---
+++ disney/test.py
@@ -0,0 +1,9 @@
+# d= {}
+
+# d[2] = "hey"
+
+# print(d)
+
+t = "wassupa bistch"
+x = t.find("s",4)
+print(x)
