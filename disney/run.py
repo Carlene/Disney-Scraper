b0f1ed8403eeca6f2f652c6ad95795ef9bd0b129
Carlene <carlenew.27@gmail.com>
2022-05-17 01:02:16 -0400

    working on removing job summary

--- disney/run.py
+++ disney/run.py
@@ -2,11 +2,10 @@
 import pandas as pd
 ####################### My Libraries #####################################
 from scraper import scrape_HTML
-from filter_job_results import separate
 from clean import split_and_clean
-from open_multiple_links import add_disney_url
+import organize_job_details as organize
 from open_multiple_links import grab_job_data_from_multiple_links
-from organize_job_details import map_job_details_with_qualifications
+from filter_job_results import separate
 ############################################################
 
 def create_csv(d):
@@ -18,10 +17,9 @@ if __name__ == "__main__":
     job_elements = scrape_HTML()
     job_list = split_and_clean(job_elements, "</tr>")
     paths = separate(job_list)[1]
-    links = add_disney_url(paths)
-    job_data = map_job_details_with_qualifications(links, job_list)
-    # job_data = grab_job_data_from_multiple_links(links)
+    print(organize.job_qualifications(grab_job_data_from_multiple_links(paths)))
+
     # create_csv(job_details_by_id)
     # df = create_csv(job_details_by_id)
     # print(df)
-    print(job_data)
+    # organize.map_job_details_with_qualifications(job_list)
