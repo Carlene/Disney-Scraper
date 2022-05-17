db9544c52513c0b8b873855b9de09b139e1d9ffc
Carlene <carlenew.27@gmail.com>
2022-05-16 22:22:35 -0400

    ignoring more unneccesary files

--- disney/organize_job_details.py
+++ disney/organize_job_details.py
@@ -1,11 +1,13 @@
-from scraper import scrape_HTML
 from filter_job_results import separate
-from clean import split_and_clean
 from open_multiple_links import grab_job_data_from_multiple_links
 import filter_job_results as fjr
 
-#TODO pull job details from links
-
 def map_job_details_with_qualifications(urls, jobs):
-    grab_job_data_from_multiple_links(urls)
+    all_job_details_by_id = {}
     job_details_by_id = separate(jobs)[0]
+    quals_by_id = grab_job_data_from_multiple_links(urls)
+    for id in job_details_by_id:
+        if id in quals_by_id:
+            all_job_details_by_id[id] = job_details_by_id[id]
+            all_job_details_by_id[id].append(quals_by_id[id])
+    return all_job_details_by_id
